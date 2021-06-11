2acfc17d70580dff5a0a8bb04f3058115a67cba1
Swayam Terode <swayamterodex@gmail.com>
2021-06-11 22:20:03 +0530

    Create 1.Index_Implementation.cpp

---
+++ C++/Array/1.Index_Implementation.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int Array []= {1,2,3,4,5,6};
+    cout<<Array[0]<<endl; // This will print 1
+    cout<<Array[1]<<endl; // This will print 2
+    cout<<Array[2]<<endl; // This will print 3
+    cout<<Array[3]<<endl; // This will print 4
+    cout<<Array[4]<<endl; // This will print 5
+    cout<<Array[5]<<endl; // This will print 6
+}
