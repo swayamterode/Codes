16afcba41c8d7dd1b7fe80595cad086bd6a7cd39
Swayam Terode <swayamterodex@gmail.com>
2021-06-14 10:30:03 +0530

    Create 6.Sum_of_Array.cpp

---
+++ C++/1.Array/6.Sum_of_Array.cpp
@@ -0,0 +1,10 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main (){
+    int array [10]={1,2,3,4,5,6,7,8,9,10};
+    int sum=0;
+    for (int i =0;i<11;i++){
+        sum=sum+array[i];
+    }
+cout<<"Sum of elements of array is:"<<sum<<endl;
+}
