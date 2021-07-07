c23840308e1058b4b3077fdc5c557fa6b7dbbd82
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-07-07 20:42:58 +0530

    Update 3.Is_A_Number_Prime.cpp

--- C++/0.Basics of Programming/1.Getting Started/3.Is_A_Number_Prime.cpp
+++ C++/0.Basics of Programming/1.Getting Started/3.Is_A_Number_Prime.cpp
@@ -8,7 +8,11 @@ int main(){
         int n;
         cin>>n;
         int i, flag = 0;
-
+        
+        if (n == 1 || n==0) 
+        {
+            cout<<n<<" is neither prime nor composite.\n";
+        }
         for (i = 2; i <= n / 2; ++i) {
         // condition for non-prime
         if (n % i == 0) 
@@ -17,19 +21,11 @@ int main(){
             break;
         }
         }
-
-        if (n == 1 || n==0) 
-        {
-            cout<<n<<" is neither prime nor composite.\n";
-        } 
-
-        else 
-        {
         if (flag == 0)
         cout<<"prime number.\n\n";
         
         else
         cout<<"Not a prime number.\n\n";
-        }
+        
     }
 }
