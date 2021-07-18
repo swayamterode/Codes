1f3ada562e0a6ae03f3f5e88b3d8a7d7ec04bb23
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-07-18 19:31:51 +0530

    08. Reverse A Number

---
+++ C++/0.Basics of Programming/08. Reverse A Number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    while(n > 0){
+        int remainder = n % 10 ;
+        n /=10;
+        cout<<remainder<<endl;
+    }
+return 0;
+}
