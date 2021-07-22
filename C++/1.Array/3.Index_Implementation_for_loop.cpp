3fc28b0bb6e4e3dfca5eb17aca17dd43700e4357
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-07-22 12:09:57 +0530

    Delete C++/1.Array directory

--- C++/1.Array/3.Index_Implementation_for_loop.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits\stdc++.h>
-using namespace std;
-int main (){
-    int array[5]={1,2,3,4,5};
-    for (int x:array){
-    cout<<x<<endl;
-    }
-    return 0;
-}
--- C++/1.Array/4.Index_Implementation_for_loop.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits\stdc++.h>
-using namespace std;
-int main (){
-
-int A[6]={1,2,3};
-for (int i:A){ // This is automatically transfer the values of A array into the int i 
-    cout<<i<<endl;
-}
-}
