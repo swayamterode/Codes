b93de8205f4d08a9257c97ec60dc302de4c96c9e
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-08-06 21:48:50 +0530

    03. Is A Number Prime.cpp
    Adding Line Indentation

--- C++/1. Basics of Programming/1. Getting Started/03. Is A Number Prime.cpp
+++ C++/1. Basics of Programming/1. Getting Started/03. Is A Number Prime.cpp
@@ -1,24 +1,30 @@
+/**
+ * @file 03. Is A Number Prime.cpp
+ * @author Swayam Terode
+ * @date 2021-08-06
+ * @copyright: Swayam Terode 2021
+ */
 #include <iostream>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int count=0;
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0){
-                count =count+1;
-                break;
-            }
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int count = 0;
+    for (int i = 2; i * i <= n; i++) {
+      if (n % i == 0) {
+        count = count + 1;
+        break;
+      }
     }
-    if(count==0)
+    if (count == 0)
     {
-        cout<<"prime"<<endl;
+      cout << "prime" << endl;
     }
     else
-    cout<<"not prime"<<endl;
-}
-return 0;
+      cout << "not prime" << endl;
+  }
+  return 0;
 }
