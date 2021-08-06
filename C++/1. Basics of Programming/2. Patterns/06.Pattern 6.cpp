5d5f4678ed352846c69cc1f3a524b168db46640e
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-08-06 18:49:29 +0530

    Pattern 6
    *	*	*		*	*	*	
    *	*				*	*	
    *						*	
    *	*				*	*	
    *	*	*		*	*	*

---
+++ C++/1. Basics of Programming/2. Patterns/06.Pattern 6.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+  int space = 1;
+  int stars = (n / 2) + 1;
+  for (int i = 1; i <= n ; i++) {
+    for (int j = 1; j <= stars; j++){
+      cout << "*\t";
+    }
+    for (int j = 1; j <= space; j++){
+      cout << "\t";
+    }
+    for (int j = 1; j <= stars; j++){
+      cout << "*\t";
+    }
+
+    if (i <= n / 2) {
+      space += 2;
+      stars--;
+    }
+    else {
+      space -= 2;
+      stars++;
+    }
+    cout << endl;
+  }
+  return 0;
+}
