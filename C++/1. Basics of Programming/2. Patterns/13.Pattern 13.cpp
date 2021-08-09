5badc10a45a68b3aaaaa6b0e50333b6b6f7fa2de
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-08-09 21:11:37 +0530

    Pattern 13
    1	
    1	1	
    1	2	1	
    1	3	3	1	
    1	4	6	4	1

---
+++ C++/1. Basics of Programming/2. Patterns/13.Pattern 13.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int ans = 1;
+    for (int j = 0; j <= i; j++) {
+      cout << ans << "\t";
+      ans = (ans * (i - j)) / (j + 1);
+    }
+    cout << endl;
+  }
+  return 0;
+}
