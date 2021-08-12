8674d322571863cf23b2e170f68172f24753c17a
Swayam <swayamterodex@gmail.com>
2021-08-12 21:21:11 +0530

    Print Z

---
+++ C++/Level 1/1. Basics of Programming/1. Getting Started/01. Print Z.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file Print Z.cpp
+ * @author Swayam Terode
+ * @brief  Printing Z using cout.
+ * @date 2021-08-06
+ * 
+ * @copyright: Swayam Terode 2021
+ */
+#include<iostream>
+using namespace std;
+int main() {
+  cout << "*****\n";
+  cout << "   * \n";
+  cout << "  *  \n";
+  cout << " *   \n";
+  cout << "*****\n";
+  return 0;
+}
