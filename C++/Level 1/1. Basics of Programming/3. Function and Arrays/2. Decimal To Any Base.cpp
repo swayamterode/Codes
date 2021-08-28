9ea02996397a153558cb1652486b05487b259a24
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-08-28 22:04:38 +0530

    Decimal To Any Base

---
+++ C++/Level 1/1. Basics of Programming/3. Function and Arrays/2. Decimal To Any Base.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int dtob(int n, int b) { // dtob= Decimal to any base.
+  int power = 1;
+  int ans = 0;
+  while (n > 0) {
+    int digit = n % b;
+    ans += (digit * power);
+    power *= 10;
+    n = n / b;
+  }
+  return ans;
+}
+int main() {
+  int n, b;
+  cin >> n >> b;
+  cout << dtob(n, b);
+  return 0;
+}
