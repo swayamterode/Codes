ba1d9a3b3ccd0de26eaf9a6bff3958606a9c90aa
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-09-01 01:29:50 +0530

    Reverse An Array

---
+++ C++/Level 1/1. Basics of Programming/3. Function and Arrays/12. Reverse An Array.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void reverse(int* arr, int n) { //reverse function
+  int i = 0;
+  int j = n - 1;
+  while (i < j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+
+    i++;
+    j--;
+  }
+}
+
+void display(int* arr, int n) { //display function
+  for (int i = 0 ; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  int* arr = new int[n];
+  for (int i = 0 ; i < n; i++) {
+    cin >> arr[i];
+  }
+  reverse(arr, n);
+  display(arr, n);
+}
