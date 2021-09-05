69b72ebf5706ea04f746751fdfe84e120564e8d1
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-09-05 23:58:17 +0530

    Create 05. Any Base Addition.cpp

---
+++ C++/Level 1/1. Basics of Programming/3. Function and Arrays/05. Any Base Addition.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int getSum(int b, int n1, int n2) {
+  int carry = 0;
+  int power = 1;
+  int ans = 0;
+
+  while (n1 !=  0 || n2 != 0 || carry != 0) {
+    int d1 = n1 % 10; //extracting digits
+    int d2 = n2 % 10;
+    int d = d1 + d2 + carry; //adding up extracted digits
+    carry = d / b; //storing carry for next addition
+    d = d % b;
+    ans += d * power;
+    power *= 10;
+    n1 /= 10;
+    n2 /= 10;
+  }
+  return ans;
+}
+int main() {
+  int b, n1, n2;
+  cin >> b >> n1 >> n2;
+  cout << getSum(b, n1, n2) << endl;
+}
