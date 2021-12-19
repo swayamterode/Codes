415f3e185608524cfeaa17899342ea6dbd315076
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-12-19 17:17:41 +0530

    Inorder Traversal

---
+++ C++/Concepts/Tree/Inorder_Traversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file Inorder_Traversal.cpp
+ * @author Swayam Terode (swayamterodex@gmail.com)
+ * @brief Inorder traversal of Binary Tree.
+ * @version 0.1
+ * @date 2021-12-19
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+Node *newNode(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void printInorder(struct Node *node)
+{
+    if (node == NULL)
+        return;
+
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+int main()
+{
+    struct Node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    cout << "\nThe Inorder traversal of Binary Tree is: ";
+    printInorder(root);
+    return 0;
+}
