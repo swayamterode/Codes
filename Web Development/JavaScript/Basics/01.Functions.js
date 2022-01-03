5bdf29117f029401150e611d7e08ea529a65069c
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-01-04 00:14:42 +0530

    Functions

---
+++ Web Development/JavaScript/Basics/01.Functions.js
@@ -0,0 +1,8 @@
+console.log("\nThis is an example of function\n");
+function sayHello(param) {
+  console.log("Hello World!");
+  console.log("Parameter Received is:", param);
+}
+sayHello(007);
+sayHello("This is a String!");
+sayHello([1, 2, 3, 4, 5]);
