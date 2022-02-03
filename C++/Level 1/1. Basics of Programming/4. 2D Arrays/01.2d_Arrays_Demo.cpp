240fdb11e4086093fb29f6933d2f26326fb6733e
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-02-04 03:01:53 +0530

    Update 01.2d_Arrays_Demo.cpp

--- C++/Level 1/1. Basics of Programming/4. 2D Arrays/01.2d_Arrays_Demo.cpp
+++ C++/Level 1/1. Basics of Programming/4. 2D Arrays/01.2d_Arrays_Demo.cpp
@@ -1,21 +1,32 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-  int n, m;
-  cin >> n >> m;
-  int arr[n][m]; //Array initialization
-  //Input from user
-  for (int i = 0; i < n ; i++) {
-    for (int j = 0; j < m ; j++) {
-      cin >> arr[i][j];
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> arr;
+    //input
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> sarr;
+        for (int j = 0; j < m; j++)
+        {
+            int element;
+            cin >> element;
+            sarr.push_back(element);
+        }
+        arr.push_back(sarr);
     }
-  }
-  //Output of array
-  for (int i = 0 ; i < n ; i ++) {
-    for (int j = 0 ; j < m; j++) {
-      cout << arr[i][j] << " ";
+
+    //Output
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  return 0;
+    return 0;
 }
