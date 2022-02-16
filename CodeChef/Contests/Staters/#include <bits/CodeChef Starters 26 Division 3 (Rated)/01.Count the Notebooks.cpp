aa3db2d2bb789c9421253310d2b9e49290de4d5c
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-02-17 02:08:46 +0530

    NOTEBOOK

---
+++ CodeChef/Contests/Staters/#include <bits/CodeChef Starters 26 Division 3 (Rated)/01.Count the Notebooks.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solve() {
+ int n;//pulp
+ cin>>n;   
+ int pages = n*1000;
+ int notebook = pages / 100;
+ cout<< notebook<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
