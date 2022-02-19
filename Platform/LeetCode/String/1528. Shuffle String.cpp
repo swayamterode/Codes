bae9607c71de03cf5b48eb9bd4b44ca9770e874e
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-02-19 23:59:04 +0530

    Update 1528. Shuffle String.cpp

--- Platform/LeetCode/String/1528. Shuffle String.cpp
+++ Platform/LeetCode/String/1528. Shuffle String.cpp
@@ -1,3 +1,4 @@
+//Brute force
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
@@ -6,5 +7,17 @@ public:
             s[indices[i]] = ans[i];
         }
         return s;
+    }
+};
+
+//Optimal
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        vector <char> orignal(s.size());
+        for(int i = 0; i<indices.size() ; i++) {
+            orignal[indices[i]] = s[i];
+        }
+        return string (orignal.begin(),orignal.end());
     }
 };
