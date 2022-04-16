50c3cb36799c7c8dee63f423ffd39d167f928979
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-04-16 18:30:52 +0530

    ex11: print every item on the list

---
+++ DEV/neoG.camp/levelzero/ex11-print_every_item_on_the_list.js
@@ -0,0 +1,5 @@
+var groceryList = ["Milk", "Flowers", "Rice Bag", "Sugar", "Vegies"];
+console.log("The Element of the Grocery are: ");
+for (var i = 0; i < groceryList.length; i++) {
+  console.log(groceryList[i]);
+}
