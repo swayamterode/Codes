b0e514ce95235e367356ddf2c9ea2de4d5a5286a
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-04-16 18:07:08 +0530

    ex10: list grocery items to buy

---
+++ DEV/neoG.camp/levelzero/ex10-list_grocery _items_to_buy.js
@@ -0,0 +1,6 @@
+var groceryList = ["Rice", "Daal", "Carrot", "Water Can"];
+
+console.log("First Item of the Grocery is: ",groceryList[0]);
+console.log("Third Item of Grocery is: ",groceryList[2]);
+var lastItem = groceryList.length;
+console.log("Last Item of Grocery is: ",groceryList[lastItem - 1]);
