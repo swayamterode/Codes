1600036feb5790f7c17394f56b9fe54ff4a83e87
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-04-17 13:39:05 +0530

    ex12: club info about a superhero together

---
+++ DEV/neoG.camp/levelzero/ex12-club_info_about_a_superhero_together.js
@@ -0,0 +1,15 @@
+var superman = {
+  name: "Superman",
+  power: "Strongest!",
+  range: "100",
+  height: "Tall",
+};
+var batman = {
+  name: "Batman",
+  power: " Wings",
+  range: "1000",
+  height: "110",
+};
+console.log(superman.name);
+console.log(batman.name);
+console.log("Is Batmans Range is greater than Superman: ",superman.range<batman.range)
