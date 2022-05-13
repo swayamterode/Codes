a213610a39807c5ddb9651222a3a222126e89787
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-05-13 12:17:18 +0530

    Experiment 4a

---
+++ Languages/Shell-Scripting/Experiments/Experiment4a.sh
@@ -0,0 +1,16 @@
+#To print given number in reverse order
+# Swayam Terode SITA45
+
+echo "Enter a number"
+read n
+sd=0
+rev=0
+
+while [ $n -gt 0 ]
+do
+    sd=$(( $n % 10 ))
+    rev=`expr $rev \* 10 + $sd` 
+    n=$(( $n / 10 ))
+done
+
+echo "Reverse number of entered digit is $rev"
