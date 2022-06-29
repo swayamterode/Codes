d596c7c677c1c9a4e33dc7dcc6b1f4ce2a5f458c
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-06-29 21:53:51 +0530

    Const keyword

---
+++ Languages/JavaScript/pages/const.js
@@ -0,0 +1,16 @@
+//you can create a constant array:
+const studentList = ["Swayam", "Ram", "Sham"];
+
+//You can change array elemts
+studentList[0] = "Tim";
+
+//you can push elements to the array
+studentList.push("Aditya");
+
+//but you can't create new array with smae arrayname
+
+//studentList = ["Tommy", "Raj"]; //Error
+
+const car = { type: "Fiat", model: "500", color: "white" };
+
+//car = { type: "Volvo", model: "EX60", color: "red" }
