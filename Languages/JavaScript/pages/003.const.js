5205159ada744a89f2d3bc0a9cf037c292df8147
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-06-30 21:28:04 +0530

    const

---
+++ Languages/JavaScript/pages/003.const.js
@@ -0,0 +1,21 @@
+//you can create a constant array:
+const studentList = ["Swayam", "Ram", "Sham"];
+
+//You can change array elemts
+studentList[0] = "Tim";
+
+//you can push elements to the array
+studentList.push("Aditya");
+
+//but you can't create new array with smae arrayname
+
+//studentList = ["Tommy", "Raj"]; //Error
+
+const car = { type: "Fiat", model: "500", color: "white" };
+
+//car = { type: "Volvo", model: "EX60", color: "red" }
+
+// decalre constants
+const pi = 3.14;
+//pi = 3.1451; gives error
+console.log(pi);
