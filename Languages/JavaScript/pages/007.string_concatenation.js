4af94731077cdf7c5ea5109b457ab4a3c8a7b19f
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-07-01 17:20:15 +0530

    String Concatenation

---
+++ Languages/JavaScript/pages/007.string_concatenation.js
@@ -0,0 +1,22 @@
+//playing with strings
+let string1 = "Sunny";
+let string2 = "Deol";
+let fullName = string1 + " " + string2;
+console.log("Type:", typeof fullName);
+console.log(fullName);
+console.log("\n\n\n");
+
+//string->Number
+string1 = "10";
+string2 = "30";
+let newString = string1 + string2; //this joins the two strings!
+console.log("Type:", typeof newString);
+console.log("String:", newString);
+console.log("\n\n\n");
+
+string1 = "10";
+string2 = "30";
+newString = +string1 + +string2; //this adds two strings numbers!
+console.log("This is an Number:", newString);
+console.log("Type:", typeof newString);
+console.log("\n\n\n");
