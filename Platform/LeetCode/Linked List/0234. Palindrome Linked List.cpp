97c645027d5343dd4fe15d33d0232fd360efe885
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-07-03 14:38:28 +0530

    234. Palindrome Linked List

---
+++ Platform/LeetCode/Linked List/0234. Palindrome Linked List.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return true;
+
+        ListNode *fast = head;
+        ListNode *slow = head;
+        // middle element of the ll
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        //MAIN CODE-----------Palindrome
+        slow->next = reverseList(slow->next);
+        slow = slow->next;
+
+        while (slow != NULL)
+        {
+            if (head->val != slow->val)
+                return false;
+
+            head = head->next;
+            slow = slow->next;
+        }
+        return true;
+    }
+    // reverse a ll
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *pre = NULL;
+        ListNode *next = NULL;
+        while (head != NULL)
+        {
+            next = head->next;
+            head->next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+};
