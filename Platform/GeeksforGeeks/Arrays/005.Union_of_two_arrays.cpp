747c69bbb17eb9912fac577697fce7d546b0f4af
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-07-05 01:08:03 +0530

    Union of two arrays

---
+++ Platform/GeeksforGeeks/Arrays/005.Union_of_two_arrays.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template in C++
+
+class Solution{
+    public:
+    int doUnion(int a[], int n, int b[], int m)  {
+        unordered_set<int> mp;
+        for(int i = 0; i<n; ++i) {
+            mp.insert(a[i]);
+        }
+        
+        for(int i = 0; i<m; ++i) {
+            mp.insert(b[i]);
+        }
+        return mp.size();
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];
+	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
