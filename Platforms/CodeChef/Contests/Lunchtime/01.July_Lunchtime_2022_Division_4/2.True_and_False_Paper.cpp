e51ca08d72f9d09bfe4ba2fc40eb6ed5198c5cbd
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-07-18 00:07:36 +0530

    True and False Paper

---
+++ Platforms/CodeChef/Contests/Lunchtime/01.July_Lunchtime_2022_Division_4/2.True_and_False_Paper.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST()                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define forn(i, n) for (int i = 0; i < int(n); i++) // for loop from 0 to n-1
+#define sz(v) (int)v.size()                         // size of vector or array
+#define all(v) v.begin(), v.end()                   // all of vector or array
+#define Mod 1e9 + 7                                 // mod value
+#define eb emplace_back                             // emplace back is used to add element in vector
+#define mp make_pair                                // used to make pair of two elements
+#define pb push_back                                // used to add element in vector
+int solve()
+{
+    int n, k;
+    cin >> n >> k;
+    int bob = abs(n - k);
+    cout << bob << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
