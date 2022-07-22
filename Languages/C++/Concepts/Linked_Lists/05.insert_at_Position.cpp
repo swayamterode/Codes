075210102ba76d628effbc55c4f335b5fbbfec37
Swayam Terode <swayam.programmer@gmail.com>
2022-07-23 01:39:53 +0530

    Added Addition of Submatrix

---
+++ Languages/C++/Concepts/Linked_Lists/05.insert_at_Position.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+void insertAtPosition(Node *&head, int position, int data)
+{
+    Node *newNode = new Node(data);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    int count = 0;
+    while (temp->next != NULL && count < position - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+void printList(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = new Node(100);
+
+    return 0;
+}
