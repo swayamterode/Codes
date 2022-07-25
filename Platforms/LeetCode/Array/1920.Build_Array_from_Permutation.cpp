90fba81b8b19f01c398189dc4f646c851aba9dfb
Swayam Terode <swayam.programmer@gmail.com>
2022-07-26 01:54:30 +0530

    Optimal Solution

--- Platforms/LeetCode/Array/1920.Build_Array_from_Permutation.cpp
+++ Platforms/LeetCode/Array/1920.Build_Array_from_Permutation.cpp
@@ -1,10 +1,33 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> buildArray(vector<int>& nums) {
+    vector<int> buildArray(vector<int> &nums)
+    {
         vector<int> ans;
-        for(int i = 0; i< nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
+        {
             ans.push_back(nums[nums[i]]);
         }
         return ans;
+    }
+};
+
+// Optimal Solution
+
+class Solution
+{
+public:
+    vector<int> buildArray(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] += n * (nums[nums[i]] % n);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] /= n;
+        }
+        return nums;
     }
 };
