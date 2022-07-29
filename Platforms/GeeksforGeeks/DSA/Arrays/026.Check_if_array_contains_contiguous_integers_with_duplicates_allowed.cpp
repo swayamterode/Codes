834f812698945a29c5d2b0b7ee4929166f928a89
Swayam Terode <swayam.programmer@gmail.com>
2022-07-29 18:49:00 +0530

    Check if array contains contiguous integers with duplicates

---
+++ Platforms/GeeksforGeeks/DSA/Arrays/026.Check_if_array_contains_contiguous_integers_with_duplicates_allowed.cpp
@@ -0,0 +1,47 @@
+// C++ implementation to check whether the array
+// contains a set of contiguous integers
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    // Function to check whether the array contains
+    // a set of contiguous integers
+    bool areElementsContiguous(int arr[], int n)
+    {
+	// Complete the function
+	sort(arr, arr+n);
+	for(int i = 1; i<n; i++){
+	    if(arr[i] - arr[i-1] >1)
+	    return false;
+	}
+	return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n + 1];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        if (ob.areElementsContiguous(arr, n))
+            cout << "Yes" <<endl;
+        else
+            cout << "No" <<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
